d819f84f5232a0367eca20c0f62730ab2cb789b5
YevheniyDev <yevheniy.dev@gmail.com>
2018-05-29 19:09:26 +0300

    Yevheniy
    Install Twig

--- index.php
+++ index.php
@@ -1,25 +1,10 @@
 <?php
-// Соединяемся, выбираем базу данных
-$link = mysqli_connect('localhost', 'root', '', 'phpm')  or die('Не удалось соединиться: ' . mysqli_error());
-echo 'Соединение успешно установлено';
 
-// Выполняем SQL-запрос
-$result = $link->query("SELECT * FROM user", MYSQLI_USE_RESULT) or die('Запрос не удался: ' . mysqli_error());
+require_once '/vendor/autoload.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
 
-// Выводим результаты в html
-echo "<table>\n";
-while ($line = $result -> fetch_assoc()) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
-}
-echo "</table>\n";
-
-// Освобождаем память от результата
-mysqli_free_result($result);
-
-// Закрываем соединение
-mysqli_close($link);
+$template = $twig->loadTemplate('index.html');
+echo $template->render($array());
 ?>
