67e7964a8306ba5189c20b067cdb36fd84100e9a
YevheniyDev <yevheniy.dev@gmail.com>
2018-05-29 20:47:32 +0300

    Yevheniy
    Proba#1

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <?php
 
 require_once('vendor/autoload.php');
-Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader);
+$twig = new Twig_Environment($loader, array());
 
-$template = $twig->loadTemplate('index.html');
-echo $template->render($array());
-?>
+echo $twig->render('index.php', array(
+  'name' => 'Fabien'
+
+));
+
+$link = mysqli_connect('localhost', 'root', '', 'phpm')  or die('Не удалось соединиться: ' . mysqli_error());
+echo 'Соединение успешно установлено';
+
+// Выполняем SQL-запрос
+// $query = $query("SELECT * FROM user", MYSQLI_USE_RESULT);
+$result = $link->query("SELECT * FROM user", MYSQLI_USE_RESULT) or die('Запрос не удался: ' . mysqli_error());
+
+// Выводим результаты в html
+
+
+while ($line = $result -> fetch_assoc()) {
+
+    foreach ($line as $col_value) {
+        echo'<br>'. "$col_value";
+    }
+
+}
+
+
+
+// Освобождаем память от результата
+mysqli_free_result($result);
+
+// Закрываем соединение
+mysqli_close($link);
---
+++ templates/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Страница шаблонизатора</title>
+  </head>
+  <body>
+    <h1>Привет {{base}}
+    </h1>
+  </body>
+</html>
